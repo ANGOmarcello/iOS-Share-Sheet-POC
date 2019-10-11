0383f9a6842e1c7bbf30e5de90eec813955ff800
Angelo Cammalleri <angelocammalleri@me.com>
2019-10-11 14:28:10 +0200

    Initial Commit

---
+++ Share-POC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Share-POC
+//
+//  Created by Angelo Cammalleri on 11.10.19.
+//  Copyright © 2019 Angelo Cammalleri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
